8be1d9ffda49ae9a4622c6196bda91320f5bc38b
Lanna-27 <lalacast2000@gmail.com>
2023-05-20 15:38:57 -0500

    Script archivos proy. y archivo para el ejemplo

--- ArchProyectados/archProy.c
+++ ArchProyectados/archProy.c
@@ -1,55 +1,91 @@
-//Hecho con chatGPT
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
 
-#define FILE_LENGTH 0x100
+#define SIZE 200
+/* Offset del inicio de la parte a leer*/
+#define OFFSETR 80 
+/* Offset del inicio de la parte a escribir*/
+#define OFFSETW 150
 
-int main(int argc, char* const argv[]) {
-    int fd, count;
-    char* file_memory;
-    char str[] = "Hello, world!\n";
-    struct stat statbuf;
+/*
+struct stat {
+               dev_t     st_dev;         // ID of device containing file
+               ino_t     st_ino;         // Inode number
+               mode_t    st_mode;        // File type and mode
+               nlink_t   st_nlink;       // Number of hard links
+               uid_t     st_uid;         // User ID of owner
+               gid_t     st_gid;         // Group ID of owner
+               dev_t     st_rdev;        // Device ID (if special file)
+               off_t     st_size;        // Total size, in bytes
+               blksize_t st_blksize;     // Block size for filesystem I/O
+               blkcnt_t  st_blocks;      // Number of 512B blocks allocated
 
-    // Abrir el archivo para escritura
-    fd = open("archivo.txt", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
-    if (fd == -1) {
-        perror("open");
-        exit(EXIT_FAILURE);
+               // Since Linux 2.6, the kernel supports nanosecond
+                  precision for the following timestamp fields.
+                  For the details before Linux 2.6, see NOTES.
+
+               struct timespec st_atim;  // Time of last access
+               struct timespec st_mtim;  // Time of last modification
+               struct timespec st_ctim;  // Time of last status change
+
+           #define st_atime st_atim.tv_sec      // Backward compatibility
+           #define st_mtime st_mtim.tv_sec
+           #define st_ctime st_ctim.tv_sec
+           };
+*/
+
+int main() {
+  int fd, tam_fich;
+  struct stat sb;
+  char *file_data;
+  off_t offsetRead = OFFSETR;
+  off_t offsetWrite = OFFSETW;
+  size_t size = SIZE;    // Tamaño de la parte a leer
+
+  // Abrir el archivo en modo lectura y escritura
+  fd = open("archivo.txt", O_RDWR);
+  if (fd == -1) {
+    perror("open");
+    exit(1);
+  }
+
+  // Obtener información sobre el archivo
+  if (fstat(fd, &sb) == -1) {
+    perror("fstat");
+    exit(1);
+  }
+
+  // Proyectar el archivo en memoria
+  // void * mmap (void *address, size_t length, int protect, int flags, int
+  // filedes, off_t offset);
+  tam_fich=lseek (fd, 0, SEEK_END);
+  file_data = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (file_data == MAP_FAILED) {
+    perror("mmap");
+    exit(1);
+  }
+
+  // Cerrar el descriptor de archivo, ya que no lo necesitamos más
+  close(fd);
+  
+  //  Escribir "todas los 1s como as" en el archivo proyectado en memoria    
+    for (int i=0; i<tam_fich; i++ ){
+      if (file_data[i]=='1') file_data[i]='a';
     }
+    
+  // Leer una parte del archivo proyectado
+  printf("Parte aleatoria del archivo proyectado:\n%.*s\n", (int)size,
+         file_data + offsetRead);
 
-    // Obtener el tamaño del archivo
-    if (fstat(fd, &statbuf) == -1) {
-        perror("fstat");
-        exit(EXIT_FAILURE);
-    }
+  // Liberar el archivo proyectado en memoria
+  if (munmap(file_data, sb.st_size) == -1) {
+    perror("munmap");
+    exit(1);
+  }
 
-    // Proyectar el archivo en memoria
-    file_memory = mmap(0, FILE_LENGTH, PROT_WRITE, MAP_SHARED, fd, 0);
-    if (file_memory == MAP_FAILED) {
-        perror("mmap");
-        exit(EXIT_FAILURE);
-    }
-
-    // Escribir datos en el archivo proyectado
-    for (count = 0; count < sizeof(str); ++count) {
-        file_memory[count] = str[count];
-    }
-
-    // Desproyectar el archivo en memoria
-    if (munmap(file_memory, FILE_LENGTH) == -1) {
-        perror("munmap");
-        exit(EXIT_FAILURE);
-    }
-
-    // Cerrar el archivo
-    if (close(fd) == -1) {
-        perror("close");
-        exit(EXIT_FAILURE);
-    }
-
-    return 0;
+  return 0;
 }
