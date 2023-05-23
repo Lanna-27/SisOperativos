261424a2ddc3dd7d4f2827afecfcccc099878d43
Lanna-27 <lalacast2000@gmail.com>
2023-05-22 22:27:30 -0500

    Mejora respecto al tamaño del archivo

--- ArchProyectados/archProy.c
+++ ArchProyectados/archProy.c
@@ -12,33 +12,6 @@
 /* Offset del inicio de la parte a escribir*/
 #define OFFSETW 150
 
-/*
-struct stat {
-               dev_t     st_dev;         // ID of device containing file
-               ino_t     st_ino;         // Inode number
-               mode_t    st_mode;        // File type and mode
-               nlink_t   st_nlink;       // Number of hard links
-               uid_t     st_uid;         // User ID of owner
-               gid_t     st_gid;         // Group ID of owner
-               dev_t     st_rdev;        // Device ID (if special file)
-               off_t     st_size;        // Total size, in bytes
-               blksize_t st_blksize;     // Block size for filesystem I/O
-               blkcnt_t  st_blocks;      // Number of 512B blocks allocated
-
-               // Since Linux 2.6, the kernel supports nanosecond
-                  precision for the following timestamp fields.
-                  For the details before Linux 2.6, see NOTES.
-
-               struct timespec st_atim;  // Time of last access
-               struct timespec st_mtim;  // Time of last modification
-               struct timespec st_ctim;  // Time of last status change
-
-           #define st_atime st_atim.tv_sec      // Backward compatibility
-           #define st_mtime st_mtim.tv_sec
-           #define st_ctime st_ctim.tv_sec
-           };
-*/
-
 int main() {
   int fd, tam_fich;
   struct stat sb;
@@ -53,18 +26,11 @@ int main() {
     perror("open");
     exit(1);
   }
-
-  // Obtener información sobre el archivo en una estructura tipo stat
-  if (fstat(fd, &sb) == -1) {
-    perror("fstat");
-    exit(1);
-  }
-
   // Proyectar el archivo en memoria
   // void * mmap (void *address, size_t length, int protect, int flags, int filedes, off_t offset);
 
   tam_fich = lseek (fd, 0, SEEK_END); //encontrar el tamaño del archivo
-  file_data = mmap(NULL, sb.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  file_data = mmap(NULL, tam_fich, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
   if (file_data == MAP_FAILED) {
     perror("mmap");
     exit(1);
